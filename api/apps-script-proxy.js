e13290f147da7a764f71267d2343454385e4c955
Ahmed5427 <eng.fathy998@gmail.com>
2025-07-30 12:51:58 +0300

    Update apps-script-proxy.js

--- api/apps-script-proxy.js
+++ api/apps-script-proxy.js
@@ -14,7 +14,7 @@ export default async function handler(req, res) {
   }
 
   try {
-    const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzRkmSC2DhjVMWzgRkdd-UubJcUksuEzCPuol-ZbkkRkQGPzoWXfn4oUkBrd89PUWXm/exec'; // Replace with your actual URL
+    const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbyuq1A0-PYojbyjPGfZK_8wqNJRw2iDoBrGGTUaz3xYX94qnEFG8S7Ew2Jdx83VJtjw/exec'; // Replace with your actual URL
     
     console.log('Proxying request to Apps Script:', APPS_SCRIPT_URL);
     console.log('Request data:', new URLSearchParams(req.body).toString());
